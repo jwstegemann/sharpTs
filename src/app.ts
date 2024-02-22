6200c7b8b7ee945cab0a17240bb7e87e51850533
Jens Stegemann <jwstegemann@gmail.com>
2024-02-22 13:10:17 +0100

    like aws

--- src/app.ts
+++ src/app.ts
@@ -3,30 +3,72 @@ import sharp from 'sharp';
 import opentype from 'opentype.js';
 import LineBreaker from 'linebreak';
 import fs from "fs";
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-const FONT_PATH = "./"; // process.env.FONT_PATH!;
+const AWS_REGION = "us-east-1";
+
+export const s3 = new S3Client({ region: AWS_REGION});
+
+const FONT_PATH = "fonts"; // process.env.FONT_PATH!;
 
 const fontCache = new Map<string, opentype.Font>();
 
-function getFont(name: string) {
-  if (!fontCache.has(name)) {
-    fontCache.set(name, opentype.loadSync(FONT_PATH + name));
+// Helper function to convert a stream to a buffer
+export async function s3ToBuffer(stream) {
+  const chunks: any[] = [];
+  for await (const chunk of stream) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
   }
+  return Buffer.concat(chunks);
+}
+
+async function getFont(name: string): Promise<opentype.Font> {
+  if (!fontCache.has(name)) {
+      const key = `${FONT_PATH}/${name}`;
+
+      const { Body } = await s3.send(new GetObjectCommand({
+          Bucket: "talemold-models",
+          Key: key,
+      }));
+
+      // Convert the S3 object stream to a buffer
+      const fontBuffer = new Uint8Array(await s3ToBuffer(Body)).buffer;
+
+      fontCache.set(name, opentype.parse(fontBuffer));
+  }
+
   return fontCache.get(name)!;
 }
 
+type TextBoxPosition = "NW" | "N" | "NE" | "SW" | "S" | "SE"
+type TextBoxWidth = 1 | 2 | 3 | 4
 
-function renderText(text: string, fontName: string, fontSize: number, lineHeight: number, width: number, left: number, top: number) {
+type TextBoxOptions = {
+  position: TextBoxPosition,
+  width: TextBoxWidth,
+  fontName: string,
+  fontSize: number,
+  lineHeight: number,
+  defs?: string,
+  styleBox: string,
+  styleText: string,
+  paddingX?: number,
+  paddingY?: number,
+  marginY?: number,
+  marginX?: number
+}
+
+async function renderText(text: string, fontName: string, fontSize: number, lineHeight: number, width: number, left: number, top: number) {
   const breaker = new LineBreaker(text);
 
-  const font = getFont(fontName);
-  const startY = top + fontSize * font.ascender/font.unitsPerEm;
+  const font = await getFont(fontName);
+  const startY = top + fontSize * font.ascender / font.unitsPerEm;
   const paths: string[] = [];
 
   function addPath(line: string) {
-//    console.log("pushing ", line);
-    const path: string = font.getPath(line, left, startY + paths.length  * lineHeight, fontSize).toSVG(2).replace('<path', '<path class="text"');
-    paths.push(path);
+      //    console.log("pushing ", line);
+      const path: string = font.getPath(line, left, startY + paths.length * lineHeight, fontSize).toSVG(2).replace('<path', '<path class="text"');
+      paths.push(path);
 
   }
 
@@ -62,50 +104,31 @@ function renderText(text: string, fontName: string, fontSize: number, lineHeight
 
   console.log(paths.length);
 
-  const height =  fontSize * (font.ascender/font.unitsPerEm -  font.descender/font.unitsPerEm) + (paths.length-1) * lineHeight;
+  const height = fontSize * (font.ascender / font.unitsPerEm - font.descender / font.unitsPerEm) + (paths.length - 1) * lineHeight;
 
   return {
-    paths,
-    height
+      paths,
+      height
   };
 
 }
 
-type TextBoxPosition = "NW" | "N" | "NE" | "SW" | "S" | "SE"
-type TextBoxWidth = 1 | 2 | 3 | 4
 
-
-type TextFieldOptions = {
-  position: TextBoxPosition,
-  width: TextBoxWidth,
-  fontName: string,
-  fontSize: number,
-  lineHeight: number,
-  defs?: string,
-  styleBox: string,
-  styleText: string,
-  paddingX?: number,
-  paddingY?: number,
-  marginY?: number,
-  marginX?: number
-}
-
-
-function buildSvg(text: string, width: number, options: TextFieldOptions) {
-
-  console.log("options.paddingX", options.paddingX);
-  console.log("options.marginX", options.marginX);
-
-  const boxWidth = width - (options.marginX || 0)*2;
+async function buildSvg(text: string, width: number, options: TextBoxOptions) {
+  const boxWidth = width - (options.marginX || 0) * 2;
   const textWidth = boxWidth - (options.paddingX || 0) * 2;
   const boxLeft = (options.marginX || 0);
   const boxTop = (options.marginY || 0);
   const textLeft = boxLeft + (options.paddingX || 0);
   const textTop = boxTop + (options.paddingY || 0);
 
-  const {paths, height : textHeight} = renderText(text, options.fontName, options.fontSize, options.lineHeight, textWidth, textLeft, textTop);
+  if (!options.fontName?.length) throw Error("no fontName in TextBoxOptions");
+  if (!options.fontSize) throw Error("no fontSize in TextBoxOptions");
+  if (!options.lineHeight) options.lineHeight = Math.ceil(options.fontSize * 1.2);
+
+  const { paths, height: textHeight } = await renderText(text, options.fontName, options.fontSize, options.lineHeight, textWidth, textLeft, textTop);
   const boxHeight = textHeight + (options.paddingY || 0) * 2;
-  const svgHeight = Math.ceil(boxHeight + (options.marginY || 0) *2);
+  const svgHeight = Math.ceil(boxHeight + (options.marginY || 0) * 2);
 
 
   // Debugging geometrics
@@ -122,30 +145,29 @@ function buildSvg(text: string, width: number, options: TextFieldOptions) {
   */
 
   const svg = `<svg width="${width}" height="${svgHeight}" xmlns="http://www.w3.org/2000/svg">
-  <defs>${options.defs || ""}</defs>
-  <style>
-      .text { ${options.styleText} }
-      .box { ${options.styleBox} }
-      </style>
-        <rect x="${boxLeft}" y="${boxTop}" width="${boxWidth}" height="${boxHeight}" class="box"/>
-        ${paths.join()}
-    </svg>`;
+<defs>${options.defs || ""}</defs>
+<style>
+    .text { ${options.styleText || ""} }
+    .box { ${options.styleBox || ""} }
+    </style>
+      <rect x="${boxLeft}" y="${boxTop}" width="${boxWidth}" height="${boxHeight}" class="box"/>
+      ${paths.join()}
+  </svg>`;
 
   return {
-    svg: svg,
-    height: svgHeight
+      svg: svg,
+      height: svgHeight
   };
 }
 
-
-async function renderTextBox(image: sharp.Sharp, text: string, options: TextFieldOptions) {
+async function renderTextBox(image: sharp.Sharp, text: string, options: TextBoxOptions) {
   const [imageWidth, imageHeight] = await image.metadata().then(m => [m.width!, m.height!]);
   console.log("size." , imageWidth," x ", imageHeight);
 
   const width = Math.trunc(imageWidth / options.width);
   console.log("width: ", width);
 
-  const { svg, height } = buildSvg(text, width, options);
+  const { svg, height } = await buildSvg(text, width, options);
 
   let top, left;
 
@@ -171,13 +193,13 @@ async function renderTextBox(image: sharp.Sharp, text: string, options: TextFiel
   const image = sharp("/Users/tiberius/ai/img/bst_4/fireman_gross.png");
   //const image = await sharp("/Users/tiberius/ai/img/bst_4/tina_arielle.png");
   
-  const buffer = renderTextBox(image, "Dies ist ein Test. (Ich bin mal gespannt, wie der umgebrochen wird.) Und ob überaupt etwas passgjyiert...", {
+  const buffer = renderTextBox(image, "Dies ist ein neuer Test. (Ich bin mal gespannt, wie der umgebrochen wird.) Und ob überaupt etwas passgjyiert...", {
     position: "SE",
-    width: 3,
+    width: 2,
 //    fontName: "Roboto/Roboto-Regular.ttf",
     fontName: "KMKDSP__.ttf",
-    fontSize: 80,
-    lineHeight: 96,
+    fontSize: 96,
+    lineHeight: 124,
     defs: `
       <radialGradient id="0" cx="50%" cy="50%" r="50%" >
         <stop offset="0%" style="stop-color:rgb(245,230,147);stop-opacity:1.00" />
@@ -189,8 +211,8 @@ async function renderTextBox(image: sharp.Sharp, text: string, options: TextFiel
     styleText: "fill: black",
     paddingX: 48,
     paddingY: 48,
-    marginX: 36,
-    marginY: 36
+    marginX: 64,
+    marginY: 64
   });
   
   
